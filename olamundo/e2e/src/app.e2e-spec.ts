34ec8f6b27a8273138c340a2229b4d6744836ce3
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2020-10-12 04:46:28 -0300

    commit

--- olamundo/e2e/src/app.e2e-spec.ts
+++ olamundo/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('olamundo app is running!');
+    expect(page.getTitleText()).toEqual('Olá Mundo! app is running!');
   });
 
   afterEach(async () => {
--- olamundo/src/app/app.component.spec.ts
+++ olamundo/src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'olamundo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('olamundo');
+    expect(app.title).toEqual('Olá Mundo!');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('olamundo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Olá Mundo! app is running!');
   });
 });
