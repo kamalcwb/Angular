3932c10e0d3502a6cb89cc68a6e7d2ee5d4ac2ff
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2020-10-13 03:13:32 -0300

    commit

--- calculadora/src/app/calculadora/component/caculadora.component.spec.ts
+++ calculadora/src/app/calculadora/component/caculadora.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
 
 import { CaculadoraComponent } from './caculadora.component';
+
+import { CalculadoraService } from '../services'
 
 describe('CaculadoraComponent', () => {
   let component: CaculadoraComponent;
@@ -8,9 +10,10 @@ describe('CaculadoraComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CaculadoraComponent ]
+      declarations: [CaculadoraComponent],
+      providers: [CalculadoraService]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +25,28 @@ describe('CaculadoraComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve garantir que 1 + 4 = 5',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let soma = service.calcular(1, 4, CalculadoraService.SOMA);
+      expect(soma).toEqual(5);
+    }));
+
+  it('deve garantir que 1 - 4 = -3',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let subtracao = service.calcular(1, 4, CalculadoraService.SUBTRACAO);
+      expect(subtracao).toEqual(-3);
+    }));
+
+  it('deve garantir que 1 / 4 = 0,25',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let dividir = service.calcular(1, 4, CalculadoraService.DIVISAO);
+      expect(dividir).toEqual(0.25);
+    }));
+
+  it('deve garantir que 1 * 4 = 4',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let multiplicacao = service.calcular(1, 4, CalculadoraService.MULTIPLIACAO);
+      expect(multiplicacao).toEqual(4);
+    }));
 });
